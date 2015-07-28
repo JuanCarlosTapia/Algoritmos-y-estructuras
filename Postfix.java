3c3576c7d0ff9235ec73d08e2eb19e48ca66d7dc
JuanCarlosTapia <jc_tapia95@hotmail.com>
2015-07-28 15:38:45 -0600

    Update Postfix.java

--- Postfix.java
+++ Postfix.java
@@ -1,10 +1,10 @@
 public class postfix {
 	
+	// Atributos
+	private stack Pila; // Pila para las operaciones
 	
-	private stack Pila;
 	
-	
-	
+	// Constructor
 	public postfix() {
 		
 		Pila = new stack();
@@ -12,7 +12,7 @@ public class postfix {
 
 
 
-
+	// Gets y sets
 	public stack getPila() {
 		return Pila;
 	}
@@ -22,14 +22,14 @@ public class postfix {
 		Pila = pila;
 	}
 
-
+	//Metodo para evaluar
 	public double evaluar(String Expresion){
-		int contador = 0;
+		int contador = 0; 
 		double numero;
-		while(contador< Expresion.length()){
+		while(contador< Expresion.length()){ // Mientras no se hayan revisado todos los caracteres
 			
-			switch (Expresion.substring(contador, contador+1)){
-			case "0":
+			switch (Expresion.substring(contador, contador+1)){ // Se revisa un caracter y se actua dependiendo de lo que sea
+			case "0": //Si es un digito
 			case "1":
 			case "2":
 			case "3":
@@ -39,11 +39,12 @@ public class postfix {
 			case "7":
 			case "8":
 			case "9":
-				numero = Double.parseDouble(Expresion.substring(contador, contador+1));
+				numero = Double.parseDouble(Expresion.substring(contador, contador+1)); // Se convierte el string a Double
 				
-				Pila.push(numero);
-				
+				Pila.push(numero); // y se mete a la pila.
+ 				
 				break;
+			// Si es un operador, se sacan 2 numeros de la pila, se operan, y se vuelve a meter el resultado.
 			case "+":
 				Pila.push(Pila.pop()+Pila.pop());
 				break;
@@ -61,9 +62,9 @@ public class postfix {
 				break;
 			}
 			
-		contador++;	
+		contador++;	// Se aumenta el contador
 		}
-		return Pila.pop();
+		return Pila.pop(); // Se devuelve el resultado que es el unico elemento que quedo en la pila.
 		
 	}
 	
